7956650f17df021f7c9936c024fe75d407b498e1
Tony Piña <65735766+Tonypina@users.noreply.github.com>
2024-09-05 21:03:56 -0600

    Se cambió la tabla en la que va is_flex

--- database/migrations/2024_08_23_182128_update_lineup_players_add_column_is_flex.php
+++ database/migrations/2024_08_23_182128_update_lineup_players_add_column_is_flex.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('lineups', function (Blueprint $table) {
+        Schema::table('player_positions', function (Blueprint $table) {
             $table->boolean('is_flex')->default(false);
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('lineups', function ($table) {
+        Schema::table('player_positions', function ($table) {
             $table->dropColumn('is_flex');
         });
     }
